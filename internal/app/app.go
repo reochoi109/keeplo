d1b06cf43c1a88ee5421e9627de1e583ad90f4d5
reochoi109 <reochoi109@gmail.com>
2025-07-15 08:58:18 +0900

    feat : logging -> logger 패키지 이름 변경

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,23 +5,42 @@ import (
 	"keeplo/config"
 	"keeplo/internal/adapter/rest/router"
 	"keeplo/pkg/db/postgresql"
-	"keeplo/pkg/logging"
+	"keeplo/pkg/logger"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"go.uber.org/zap"
 )
 
 func Run() {
 	config.Init()
-	logging.Init()
+	logger.Init()
 	postgresql.Init()
 
-	logging.Log.Debug("Initializing service",
+	logger.Log.Debug("Initializing service",
 		zap.String("service mode", config.AppConfig.Mode),
 		zap.String("port", config.AppConfig.Port),
 		zap.String("log_level", config.AppConfig.LogLevel),
 		zap.String("db_address", config.AppConfig.DB.DSN()),
 	)
 
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go listenForShutdown(cancel)
+	start(ctx)
+}
+
+func start(ctx context.Context) {
 	router.Run(ctx)
+}
+
+func listenForShutdown(cancelFunc context.CancelFunc) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan
+	logger.Log.Info("shutdown service...")
+	cancelFunc()
 }
